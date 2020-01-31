bf6a38837006de525954cfcb0ddd8b1468b66ecc
nicolasnevraumont <nnevraumont@gmail.com>
2020-01-31 19:51:04 +0100

    Top menu with material - temp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-come-back';
+  navLinks: any[];
+  activeLinkIndex = 0; 
+  constructor(private router: Router) {
+    this.navLinks = [
+        {
+            label: 'My Logs',
+            link: '/lists',
+            index: 0
+        }, {
+            label: 'Add log',
+            link: '/add',
+            index: 1
+        }
+    ];
+}
+ngOnInit(): void {
+  this.router.events.subscribe((res) => {
+    console.log(this.activeLinkIndex);  
+    console.log(this.router.url);  
+    if (this.router.url === '/') {
+        this.activeLinkIndex = 0;
+      }
+      else {
+        this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === this.router.url));
+      }
+  });
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
+import { PostAddComponent } from './post-add/post-add.component';
+import { RouterModule } from '@angular/router';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsListComponent,
+    PostAddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: 'lists', component: PostsListComponent },
+      { path: 'add', component: PostAddComponent },
+      { path: '', component: PostsListComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
